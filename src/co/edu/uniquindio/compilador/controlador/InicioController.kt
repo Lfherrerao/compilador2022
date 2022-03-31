29153acd4b2e7d364471f4aae89756d058763859
lfherrerao <lfherrerao@uqvirtual.edu.co>
2022-03-30 19:26:37 -0500

    en este commit se pone en funcionamiento la tabla.

--- src/co/edu/uniquindio/compilador/controlador/InicioController.kt
+++ src/co/edu/uniquindio/compilador/controlador/InicioController.kt
@@ -1,14 +1,32 @@
 package co.edu.uniquindio.compilador.controlador
 
 import co.edu.uniquindio.compilador.lexico.AnalizadorLexico
+import co.edu.uniquindio.compilador.lexico.Token
+import javafx.collections.FXCollections
 import javafx.fxml.FXML
-import javafx.scene.control.TextArea
+import javafx.fxml.Initializable
+import javafx.scene.control.*
+import javafx.scene.control.cell.PropertyValueFactory
+import java.net.URL
+import java.util.*
 
 
-class InicioController {
+class InicioController:Initializable {
 
 
     @FXML lateinit var codigoFuente:TextArea
+    @FXML lateinit var tablaTokens:TableView<Token>
+    @FXML lateinit var columnaLexema:TableColumn<Token,String>
+    @FXML lateinit var columnaCtegoria:TableColumn<Token,String>
+    @FXML lateinit var columnaFila:TableColumn<Token,Int>
+    @FXML lateinit var columnaColumna:TableColumn<Token,Int>
+
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+        columnaLexema.cellValueFactory=PropertyValueFactory("lexema")
+        columnaCtegoria.cellValueFactory=PropertyValueFactory("categoria")
+        columnaFila.cellValueFactory=PropertyValueFactory("fila")
+        columnaColumna.cellValueFactory=PropertyValueFactory("columna")
+    }
 
     fun analizarCodigo() {
 
@@ -16,8 +34,12 @@ class InicioController {
         if (codigoFuente.text.length > 0) {
             val lexico = AnalizadorLexico(codigoFuente.text)
             lexico.analizar()
-            print(lexico.listaTokens)
+
+
+            tablaTokens.items=FXCollections.observableArrayList(lexico.listaTokens)
 
         }
     }
+
+
 }
